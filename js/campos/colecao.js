b0a6dc18ec99a0c0d98be340c4af8fc2f93ed450
Nativa <92101458+GNativa@users.noreply.github.com>
2025-06-12 09:13:59 -0400

    - Coleção de campos finalizada.

--- js/campos/colecao.js
+++ js/campos/colecao.js
@@ -2,22 +2,62 @@ class ColecaoCampos {
     static #campos = {};
 
     constructor() {
-
     }
 
-    obterCampo(id) {
-        return ColecaoCampos.#campos[id];
+    obterCampo(id = "", idAgrupado = "") {
+        const campos = ColecaoCampos.#campos;
+
+        if (!campos.hasOwnProperty(idAgrupado)) {
+            throw new Error(`Não há campos salvos com o ID agrupado "${idAgrupado}"`);
+        }
+
+        const campoObtido = campos[idAgrupado].find((campo = new Campo()) => {
+            return campo.id === id;
+        });
+
+        if (!campoObtido) {
+            throw new Error(`Não há nenhum campo salvo com o ID "${id}"`);
+        }
+
+        return campoObtido;
     }
 
-    salvarCampo(campo = Campo()) {
-        ColecaoCampos.#campos[campo.id] = campo;
+    salvarCampo(campo = new Campo()) {
+        const campos = ColecaoCampos.#campos;
+
+        if (!campos.hasOwnProperty(campo.idAgrupado)) {
+            campos[campo.idAgrupado] = [];
+        }
+
+        const listaCampos = campos[campo.idAgrupado];
+        listaCampos.push(campo);
     }
 
-    removerCampo(id) {
-        delete ColecaoCampos.#campos[id];
+    salvarCampos(campos = [new Campo()]) {
+        for (const campo of campos) {
+            this.salvarCampo(campo);
+        }
     }
 
+    removerCampo(campo = new Campo()) {
+        const campos = ColecaoCampos.#campos;
+
+        if (!campo.idAgrupado) {
+            throw new Error(`O ID agrupado do campo "${campo.id}" está vazio.`);
+        }
+
+        if (!campos.hasOwnProperty(campo.idAgrupado) || campos[campo.idAgrupado].length === 0) {
+            throw new Error(`Não há campos salvos com o ID agrupado "${campo.idAgrupado}".`);
+        }
+
+        campos[campo.idAgrupado] = campos[campo.idAgrupado].filter((elemento) => {
+            return elemento.id !== campo.id;
+        });
+    }
+
+    /*
     estaVazia() {
         return Object.keys(ColecaoCampos.#campos).length === 0;
     }
+     */
 }
