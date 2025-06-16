ddf5b9a311239b26b682151912bb5e10393e8f35
Nativa <92101458+GNativa@users.noreply.github.com>
2025-06-16 17:37:36 -0400

    - Index atualizado para usar arquivos locais.

--- js/main.js
+++ js/main.js
@@ -3,5 +3,19 @@ $(document).ready(() => {
     const colecao = new ColecaoCampos();
 
     const controlador = new Controlador(validador, colecao, new Formulario(colecao, validador));
+
     controlador.inicializar();
+
+    /*
+    try {
+        controlador.inicializar();
+    }
+    catch (e) {
+        Mensagem.exibir(
+            "Erro na inicialização",
+            `Houve um erro ao inicializar o formulário: ${e}`,
+            "ERRO"
+        );
+    }
+     */
 });
